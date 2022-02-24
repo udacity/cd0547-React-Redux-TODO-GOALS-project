f6dd118d8754288e3b01871d9fb21312202bc20f
hbkwong <>
2022-02-23 18:12:00 -0800

    Goals reducer.

--- index.js
+++ index.js
@@ -48,6 +48,17 @@ function todos(state = [], action) {
   }
 }
 
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
 const store = createStore(todos);
 
 store.subscribe(() => {
