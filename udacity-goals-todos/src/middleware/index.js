b8d4f9dfd48aa6270f6c1884a7c71cb17d0fcc8b
hbkwong <>
2022-02-23 19:24:00 -0800

    Middleware.

---
+++ udacity-goals-todos/src/middleware/index.js
@@ -0,0 +1,6 @@
+import checker from "./checker";
+import logger from "./logger";
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
+
+export default applyMiddleware(thunk, checker, logger);
